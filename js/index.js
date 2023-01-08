9eb9e62e116675799e64db7572fabbface7e8901
alysonalyson <alysonalyson@users.noreply.github.com>
2023-01-08 13:02:49 -0600

    Javascript addition
    Added JS interaction for the menu

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$("#programs").hover(
+    function(){
+        $("#programsDropdown").removeClass('hidden');
+    }, function(){
+        $("#programsDropdown").addClass('hidden');
+    }
+);
+
+$("#getInvolved").hover(
+    function(){
+        $("#getInvolvedDropdown").removeClass('hidden');
+    }, function(){
+        $("#getInvolvedDropdown").addClass('hidden');
+    }
+);
